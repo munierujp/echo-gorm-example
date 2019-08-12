7e5700357e097e7e04909cc8c7488072f4f370b3
Munieru <20086673+munierujp@users.noreply.github.com>
2019-08-12 18:30:01 +0900

    Update models

--- domain/model/user.go
+++ domain/model/user.go
@@ -2,6 +2,7 @@ package model
 
 type User struct {
 	Model
-	Name       string `json:"name"`
-	LanguageID uint   `json:"language_id"`
+	Name       string   `json:"name"`
+	LanguageID uint     `json:"language_id"`
+	Language   Language `json:"-"`
 }
