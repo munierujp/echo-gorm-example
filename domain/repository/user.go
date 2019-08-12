9ed865d5a536201311e4e0053700fd15f0295f1b
Munieru <20086673+munierujp@users.noreply.github.com>
2019-08-12 12:44:20 +0900

    Add CRUD interface (#2)
    * Add models
    
    * Add repositories

---
+++ domain/repository/user.go
@@ -0,0 +1,13 @@
+package repository
+
+import (
+	"echo-gorm-example/domain/model"
+)
+
+type UserRepository interface {
+	Add(model.User) error
+	FindByID(uint) (*model.User, error)
+	Last() (*model.User, error)
+	Update(*model.User) error
+	Delete(uint) error
+}
